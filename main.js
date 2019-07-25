c1a4d078eebeb82856f93145fa4bc250ea91c848
Colin Jaffe <balloonasaurus@gmail.com>
2019-07-25 10:37:44 -0400

    adds basic list of simple challenges

---
+++ main.js
@@ -0,0 +1,7 @@
+// Print whether a number is odd or even.
+// Print whether a number is positive or negative or 0.
+// Print someone's age given their birth year.
+// Print a fahrenheit degree from a celsius degree.
+// Print a celsius degree from a fahrenheit degree.
+// Print a random number from a min to a max inclusive.
+// 
